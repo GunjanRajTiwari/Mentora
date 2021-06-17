67bb09ea7280094af565ff04d75d9b0571020c76
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-06-17 20:00:19 +0545

    port bug

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ io.on("connection", (socket) => {
     });
 });
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
     console.log("Server is running ...");
